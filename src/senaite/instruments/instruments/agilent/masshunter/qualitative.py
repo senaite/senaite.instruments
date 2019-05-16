c3204720d6bd613d8e9faba08670d9068578643c
Mike Metcalfe <mike@webtide.co.za>
2019-05-16 14:27:41 +0200

    Reworked Qualitative exporter

--- src/senaite/instruments/instruments/agilent/masshunter/qualitative.py
+++ src/senaite/instruments/instruments/agilent/masshunter/qualitative.py
@@ -1,14 +1,20 @@
 import json
 import traceback
+import xml.etree.cElementTree as ET
 from bika.lims import api
 from bika.lims import bikaMessageFactory as _
 from bika.lims.exportimport.instruments import IInstrumentAutoImportInterface
+from bika.lims.exportimport.instruments import IInstrumentExportInterface
 from bika.lims.exportimport.instruments import IInstrumentImportInterface
 from bika.lims.exportimport.instruments.instrument import format_keyword
 from bika.lims.exportimport.instruments.resultsimport import AnalysisResultsImporter
 from bika.lims.exportimport.instruments.resultsimport import InstrumentCSVResultsFileParser
 from bika.lims.utils import t
 from DateTime import DateTime
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from senaite.core.supermodel.interfaces import ISuperModel
+from zope.component import getAdapter
+from zope.component import getUtility
 from zope.interface import implements
 
 
@@ -139,8 +145,8 @@ class qualitativeimport(object):
             results = {'errors': errors, 'log': logs, 'warns': warns}
             return json.dumps(results)
 
-        # Load the most suitable parser according to file extension/options/etc...
         parser = QualitativeParser(infile)
+
         # Load the importer
         status = ['sample_received', 'attachment_due', 'to_be_verified']
         if artoapply == 'received':
@@ -175,3 +181,88 @@ class qualitativeimport(object):
 
         results = {'errors': errors, 'log': logs, 'warns': warns}
         return json.dumps(results)
+
+
+class qualitativeexport(object):
+    implements(IInstrumentExportInterface)
+    title = "Agilent Masshunter Qualitative Exporter"
+
+    def __init__(self, context):
+        self.context = context
+        self.request = None
+
+    def Export(self, context, request):
+        tray = 1
+        norm = getUtility(IIDNormalizer).normalize
+        filename = '{}-{}.xml'.format(
+            context.getId(), norm(self.title))
+        now = str(DateTime())[:16]
+
+        root = ET.Element('SequenceTableDataSet')
+        root.set('SchemaVersion', "1.0")
+        root.set('SequenceComment', "")
+        root.set('SequenceOperator', "")
+        root.set('SequenceSeqPathFileName', "")
+        root.set('SequencePreSeqAcqCommand', "")
+        root.set('SequencePostSeqAcqCommand', "")
+        root.set('SequencePreSeqDACommand', "")
+        root.set('SequencePostSeqDACommand', "")
+        root.set('SequenceReProcessing', "False")
+        root.set('SequenceInjectBarCodeMismatch', "OnBarcodeMismatchInjectAnyway")
+        root.set('SequenceOverwriteExistingData', "False")
+        root.set('SequenceModifiedTimeStamp', now)
+        root.set('SequenceFileECMPath', "")
+
+        # for looking up "cup" number (= slot) of ARs
+        parent_to_slot = {}
+        layout = context.getLayout()
+        for item in layout:
+            p_uid = item.get('parent_uid')
+            if not p_uid:
+                p_uid = item.get('container_uid')
+            if p_uid not in parent_to_slot.keys():
+                parent_to_slot[p_uid] = int(item['position'])
+
+        rows = []
+        sequences = []
+        for item in layout:
+            # create batch header row
+            p_uid = item.get('parent_uid')
+            if not p_uid:
+                p_uid = item.get('container_uid')
+            if not p_uid:
+                continue
+            if p_uid in sequences:
+                continue
+            sequences.append(p_uid)
+            cup = parent_to_slot[p_uid]
+            rows.append({
+                'tray': tray,
+                'cup': cup,
+                'analysis_uid': getAdapter(item['analysis_uid'], ISuperModel),
+                'sample': getAdapter(item['container_uid'], ISuperModel)
+            })
+        rows.sort(lambda a, b: cmp(a['cup'], b['cup']))
+
+        cnt = 0
+        for row in rows:
+            seq = ET.SubElement(root, 'Sequence')
+            ET.SubElement(seq, 'SequenceID').text = str(row['tray'])
+            ET.SubElement(seq, 'SampleID').text = str(cnt)
+            ET.SubElement(seq, 'AcqMethodFileName').text = 'Dunno'
+            ET.SubElement(seq, 'AcqMethodPathName').text = 'Dunno'
+            ET.SubElement(seq, 'DataFileName').text = row['sample'].Title()
+            ET.SubElement(seq, 'DataPathName').text = 'Dunno'
+            ET.SubElement(seq, 'SampleName').text = row['sample'].Title()
+            ET.SubElement(seq, 'SampleType').text = row['sample'].SampleType.Title()
+            ET.SubElement(seq, 'Vial').text = str(row['cup'])
+            cnt += 1
+
+        xml = ET.tostring(root, method='xml')
+        # stream file to browser
+        setheader = request.RESPONSE.setHeader
+        setheader('Content-Length', len(xml))
+        setheader('Content-Disposition',
+                  'attachment; filename="%s"' % filename)
+        setheader('Content-Type', 'text/xml')
+        request.RESPONSE.write(xml)
